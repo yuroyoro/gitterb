268d2079267ca76bd10947eee8cebb48b7660e46
Tomohito Ozaki <ozaki@yuroyoro.com>
2012-03-07 14:09:40 +0900

    upgrade to rails 3.1.4.

--- config/application.rb
+++ config/application.rb
@@ -5,9 +5,12 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "active_resource/railtie"
 
-# If you have a Gemfile, require the gems listed there, including any gems
-# you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env) if defined?(Bundler)
+if defined?(Bundler)
+  # If you precompile assets before deploying to production, use this line
+  Bundler.require *Rails.groups(:assets => %w(development test))
+  # If you want your assets lazily compiled in production, use this line
+  # Bundler.require(:default, :assets, Rails.env)
+end
 
 module Gitterb
   class Application < Rails::Application
@@ -41,6 +44,9 @@ module Gitterb
 
     # Enable the asset pipeline
     config.assets.enabled = true
+
+    # Version of your assets, change this if you want to expire all your assets
+    config.assets.version = '1.0'
 
     # Specify browsing repository path.
     def add_repository(path,opts = {})
